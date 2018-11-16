90b4b03be4b8320e41372d24cf895f0b4a3a4a02
shehur <shehur5727@gmail.com>
2018-11-16 18:17:06 -0200

    inclusão da função soma sem parâmetros

--- test/calculosTest.php
+++ test/calculosTest.php
@@ -4,8 +4,12 @@ use PHPUnit\Framework\TestCase;
 include 'soma.php';
 
 class CalculosSoma extends TestCase {
-	public function testSoma() {
+	public function testSomaComParametros() {
 		$this->assertEquals(5, somar(2, 3));
+	}
+
+	public function testSomaSemParametros() {
+		$this->assertEquals(0, somar());
 	}
 }
 ?>
